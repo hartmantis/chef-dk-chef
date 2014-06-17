70eca33f336a3d510b9f13200ebce3388c954d93
Jonathan Hartman <j@p4nt5.com>
2014-06-17 16:13:44 -0700

    Convert spec_helper to new RSpec syntax

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,7 +6,7 @@ require 'tmpdir'
 require 'fileutils'
 
 RSpec.configure do |c|
-  c.color_enabled = true
+  c.color = true
 
   c.before(:suite) do
     COOKBOOK_PATH = Dir.mktmpdir 'chefspec'
@@ -19,20 +19,23 @@ RSpec.configure do |c|
 
   c.before(:each) do
     # Don't worry about external cookbook dependencies
-    Chef::Cookbook::Metadata.any_instance.stub(:depends)
+    allow_any_instance_of(Chef::Cookbook::Metadata).to receive(:depends)
 
     # Prep lookup() for the stubs below
-    Chef::ResourceCollection.any_instance.stub(:lookup).and_call_original
+    allow_any_instance_of(Chef::ResourceCollection).to receive(:lookup)
+      .and_call_original
 
     # Test each recipe in isolation, regardless of includes
     @included_recipes = []
-    Chef::RunContext.any_instance.stub(:loaded_recipe?).and_return(false)
-    Chef::Recipe.any_instance.stub(:include_recipe) do |i|
-      Chef::RunContext.any_instance.stub(:loaded_recipe?).with(i)
+    allow_any_instance_of(Chef::RunContext).to receive(:loaded_recipe?)
+      .and_return(false)
+    allow_any_instance_of(Chef::Recipe).to receive(:include_recipe) do |i|
+      allow_any_instance_of(Chef::RunContext).to receive(:loaded_recipe?)
+        .with(i)
         .and_return(true)
       @included_recipes << i
     end
-    Chef::RunContext.any_instance.stub(:loaded_recipes)
+    allow_any_instance_of(Chef::RunContext).to receive(:loaded_recipes)
       .and_return(@included_recipes)
   end
 
