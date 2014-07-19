7fc5f41aca4ccdf4e32f2f48b0d4e94e4b4d68b4
Jonathan Hartman <j@p4nt5.com>
2014-07-18 23:52:12 -0700

    Add a provider for RHEL platforms

---
+++ libraries/provider_chef_dk_rhel.rb
@@ -0,0 +1,79 @@
+# Encoding: UTF-8
+#
+# Cookbook Name:: chef-dk
+# Library:: provider/chef_dk_rhel
+#
+# Copyright 2014, Jonathan Hartman
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require 'chef/provider'
+require 'chef/provider/package/rpm'
+require_relative 'resource_chef_dk'
+
+class Chef
+  class Provider
+    class ChefDk < Provider
+      # A Chef provider for the Chef-DK .rpm packages
+      #
+      # @author Jonathan Hartman <j@p4nt5.com>
+      class Rhel < ChefDk
+        private
+
+        #
+        # Override the platform name used in the package URL
+        # (RHEL and RHELalikes use "el")
+        #
+        # @return [String]
+        #
+        def platform
+          'el'
+        end
+
+        #
+        # Override the platform version used in the package URL
+        # (RHEL and RHELalikes use the major version only)
+        #
+        # @return [String]
+        #
+        def platform_version
+          node['platform_version'].to_i.to_s
+        end
+
+        #
+        # Override the elements used to assemble a package file name
+        # (RHEL and RHELalikes use the pla
+        #
+        # @return [Array]
+        #
+        def package_file_elements
+          [
+            PACKAGE_NAME,
+            "#{version}.#{platform}#{platform_version}." <<
+              node['kernel']['machine']
+          ]
+        end
+
+        #
+        # Return the extension of package files used by this system
+        #
+        # @return [String]
+        #
+        def package_file_extension
+          '.rpm'
+        end
+      end
+    end
+  end
+end
---
+++ spec/libraries/provider_chef_dk_rhel_spec.rb
@@ -0,0 +1,76 @@
+# Encoding: UTF-8
+#
+# Cookbook Name:: chef-dk
+# Spec:: provider/chef_dk_rhel
+#
+# Copyright (C) 2014, Jonathan Hartman
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require_relative '../spec_helper'
+require_relative '../../libraries/provider_chef_dk_rhel'
+
+describe Chef::Provider::ChefDk::Rhel do
+  let(:platform) { {} }
+  let(:chefdk_version) { nil }
+  let(:package_url) { nil }
+  let(:new_resource) do
+    double(name: 'my_chef_dk',
+           version: chefdk_version,
+           package_url: package_url)
+  end
+  let(:provider) { described_class.new(new_resource, nil) }
+
+  before(:each) do
+    allow_any_instance_of(described_class).to receive(:node).and_return(
+      Fauxhai.mock(platform).data
+    )
+  end
+
+  describe '#platform' do
+    it 'returns "el"' do
+      expect(provider.send(:platform)).to eq('el')
+    end
+  end
+
+  describe '#platform_version' do
+    {
+      '7.0' => '7',
+      '6.5' => '6',
+      '5.10' => '5'
+    }.each do |full_version, major_version|
+      context "a centos-#{full_version} node" do
+        let(:platform) { { platform: 'centos', version: full_version } }
+
+        it 'returns the major version, "#{major_version}"' do
+          expect(provider.send(:platform_version)).to eq(major_version)
+        end
+      end
+    end
+  end
+
+  describe '#package_file_elements' do
+    let(:platform) { { platform: 'centos', version: '6.5' } }
+
+    it 'returns the elements to assemble into a RHEL file name' do
+      expected = %w(chefdk 0.2.0-2.el6.x86_64)
+      expect(provider.send(:package_file_elements)).to eq(expected)
+    end
+  end
+
+  describe '#package_file_extension' do
+    it 'returns the ".rpm" extension' do
+      expect(provider.send(:package_file_extension)).to eq('.rpm')
+    end
+  end
+end
