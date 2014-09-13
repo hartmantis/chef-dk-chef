4eae8134deb866f495611c47a81a1a91b344627b
Jonathan Hartman <j@p4nt5.com>
2014-09-12 22:22:20 -0700

    Update gem version reqs; move to Serverspec 2 beta

--- test/integration/default/serverspec/spec_helper.rb
+++ test/integration/default/serverspec/spec_helper.rb
@@ -18,13 +18,5 @@
 # limitations under the License.
 
 require 'serverspec'
-require 'pathname'
 
-include Serverspec::Helper::Exec
-include Serverspec::Helper::DetectOS
-
-RSpec.configure do |c|
-  c.before :all do
-    c.os = backend(Serverspec::Commands::Base).check_os
-  end
-end
+set :backend, :exec
