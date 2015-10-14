8ccebbae6ca667064939c739efed385dc6fb75de
Franklin Hanson <fhanson@gannett.com>
2015-10-14 00:33:02 -0400

    Rubocop style fixes
    Fix long line lengths and poor style

--- providers/gem.rb
+++ providers/gem.rb
@@ -5,12 +5,14 @@ def whyrun_supported?
 end
 
 action :install do
-  Chef::Log.debug "Installing chef gem #{new_resource.name} in #{Dir.home(new_resource.user)}"
+  Chef::Log.debug "Installing chef gem #{new_resource.name}"
   execute "chef exec gem install \
     #{new_resource.name} #{'--version' unless new_resource.version.nil?}\
     #{new_resource.version unless new_resource.version.nil?}" do
     user new_resource.user
-    environment ({'HOME' => Dir.home(new_resource.user), 'USER' => new_resource.user})
+    environment(
+      'HOME' => Dir.home(new_resource.user),
+      'USER' => new_resource.user)
     cwd Dir.home(new_resource.user)
     not_if(::File.exist?("#{Dir.home(new_resource.user)}/.chefdk/gem/ruby/\
       2.1.0/gems/#{new_resource.name}-\
@@ -26,7 +28,9 @@ action :remove do
     #{new_resource.version unless new_resource.version.nil?}" do
     user new_resource.user
     cwd Dir.home(new_resource.user)
-    environment ({'HOME' => Dir.home(new_resource.user), 'USER' => new_resource.user})
+    environment(
+      'HOME' => Dir.home(new_resource.user),
+      'USER' => new_resource.user)
   end
   new_resource.updated_by_last_action(true)
 end
