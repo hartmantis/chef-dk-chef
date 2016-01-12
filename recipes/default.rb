5e866d7f0e4dbcc3672143e201dce3065032b87c
Jonathan Hartman <j@p4nt5.com>
2016-01-12 14:04:32 -0800

    Update year to 2016

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,7 +3,7 @@
 # Cookbook Name:: chef-dk
 # Recipe:: default
 #
-# Copyright 2014-2015 Jonathan Hartman
+# Copyright 2014-2016, Jonathan Hartman
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
