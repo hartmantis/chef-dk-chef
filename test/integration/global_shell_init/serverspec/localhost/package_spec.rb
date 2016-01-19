d85445eefa1c709b412a82f2a70a5c18db3b796d
Jonathan Hartman <j@p4nt5.com>
2016-01-19 09:52:48 -0800

    Remove symlinks from serverspec tests

--- test/integration/global_shell_init/serverspec/localhost/package_spec.rb
+++ test/integration/global_shell_init/serverspec/localhost/package_spec.rb
@@ -1,1 +1,25 @@
-../../../default/serverspec/localhost/package_spec.rb
+# Encoding: UTF-8
+
+require_relative '../spec_helper'
+
+describe 'Chef-DK package' do
+  describe package('com.getchef.pkg.chefdk'), if: os[:family] == 'darwin' do
+    it 'is installed' do
+      expect(subject).to be_installed.by(:pkgutil)
+    end
+  end
+
+  # On Windows, the package name changes to reflect each ChefDK version
+  describe package('Chef Development Kit v*'), if: os[:family] == 'windows' do
+    it 'is installed' do
+      expect(subject).to be_installed
+    end
+  end
+
+  describe package('chefdk'),
+           if: %w(ubuntu debian redhat fedora).include?(os[:family]) do
+    it 'is installed' do
+      expect(subject).to be_installed
+    end
+  end
+end
