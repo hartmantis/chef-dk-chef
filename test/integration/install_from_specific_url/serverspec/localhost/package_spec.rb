4fedf785e6ae0969d9c4947498b4a6206c36f8ce
Jonathan Hartman <j@p4nt5.com>
2016-07-21 10:23:09 -0700

    Fix the failing specific source test

--- test/integration/install_from_specific_url/serverspec/localhost/package_spec.rb
+++ test/integration/install_from_specific_url/serverspec/localhost/package_spec.rb
@@ -6,13 +6,7 @@ require_relative '../spec_helper'
 describe 'chef-dk::install_from_specific_url::package' do
   describe package('chefdk') do
     it 'is installed with the right version' do
-      expect(subject).to be_installed.with_version('0.3.4-1')
-    end
-  end
-
-  describe command('/opt/chef/embedded/bin/gem list omnijack') do
-    it 'does not list Omnijack as installed' do
-      expect(subject.stdout).not_to match(/^omnijack /)
+      expect(subject).to be_installed.with_version('0.14.25-1')
     end
   end
 end
